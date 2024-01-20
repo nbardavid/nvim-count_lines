4596335c795c418ffd88dc49d426523d524ee0d4
lourio <nbabardavid@gmail.com>
2024-01-20 13:06:02 +0100

    Plugin fonctionnel

---
+++ count_line.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+function M.count_lines()
+    local buffer_id = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_clear_namespace(buffer_id, -1, 0, -1)
+    local lines = vim.api.nvim_buf_get_lines(buffer_id, 0, -1, false)
+    local trigger = 0
+    local ligne_debut = nil
+
+    for i, line in ipairs(lines) do
+        if line:match("^{$") then
+            trigger = 1
+            ligne_debut = i
+        elseif line:match("^}$") and trigger == 1 then
+            trigger = 0
+            local nbr_lignes = i - ligne_debut - 1
+            vim.api.nvim_buf_set_virtual_text(buffer_id, -1, i, {{ "//------" .. nbr_lignes .. " lignes------", "Comment" }}, {})
+        end
+    end
+end
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufRead", "TextChanged", "TextChangedI", "BufWritePost"}, {
+    pattern = "*",
+    callback = M.count_lines
+})
+
+return M
